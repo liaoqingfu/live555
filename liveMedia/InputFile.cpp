6c23eec8466ed6ad58eb3de26ea6da2d32cf8a7e
longcp <1784857911@qq.com>
2018-01-26 17:23:03 +0800

    添加注释、debug打印、调整文件格式

--- liveMedia/InputFile.cpp
+++ liveMedia/InputFile.cpp
@@ -21,92 +21,102 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #include "InputFile.hh"
 #include <string.h>
 
-FILE* OpenInputFile(UsageEnvironment& env, char const* fileName) {
-  FILE* fid;
+FILE* OpenInputFile(UsageEnvironment& env, char const* fileName)
+{
+    FILE* fid;
 
-  // Check for a special case file name: "stdin"
-  if (strcmp(fileName, "stdin") == 0) {
-    fid = stdin;
+    // Check for a special case file name: "stdin"
+    if (strcmp(fileName, "stdin") == 0) {
+        fid = stdin;
 #if (defined(__WIN32__) || defined(_WIN32)) && !defined(_WIN32_WCE)
-    _setmode(_fileno(stdin), _O_BINARY); // convert to binary mode
+        _setmode(_fileno(stdin), _O_BINARY); // convert to binary mode
 #endif
-  } else {
-    fid = fopen(fileName, "rb");
-    if (fid == NULL) {
-      env.setResultMsg("unable to open file \"",fileName, "\"");
-    }
-  }
-
-  return fid;
-}
-
-void CloseInputFile(FILE* fid) {
-  // Don't close 'stdin', in case we want to use it again later.
-  if (fid != NULL && fid != stdin) fclose(fid);
-}
-
-u_int64_t GetFileSize(char const* fileName, FILE* fid) {
-  u_int64_t fileSize = 0; // by default
-
-  if (fid != stdin) {
-#if !defined(_WIN32_WCE)
-    if (fileName == NULL) {
-#endif
-      if (fid != NULL && SeekFile64(fid, 0, SEEK_END) >= 0) {
-	fileSize = (u_int64_t)TellFile64(fid);
-	if (fileSize == (u_int64_t)-1) fileSize = 0; // TellFile64() failed
-	SeekFile64(fid, 0, SEEK_SET);
-      }
-#if !defined(_WIN32_WCE)
     } else {
-      struct stat sb;
-      if (stat(fileName, &sb) == 0) {
-	fileSize = sb.st_size;
-      }
+        fid = fopen(fileName, "rb");
+        if (fid == NULL) {
+            env.setResultMsg("unable to open file \"", fileName, "\"");
+        }
     }
-#endif
-  }
 
-  return fileSize;
+    return fid;
 }
 
-int64_t SeekFile64(FILE *fid, int64_t offset, int whence) {
-  if (fid == NULL) return -1;
+void CloseInputFile(FILE* fid)
+{
+    // Don't close 'stdin', in case we want to use it again later.
+    if (fid != NULL && fid != stdin)
+        fclose(fid);
+}
 
-  clearerr(fid);
-  fflush(fid);
+u_int64_t GetFileSize(char const* fileName, FILE* fid)
+{
+    u_int64_t fileSize = 0; // by default
+
+    if (fid != stdin) {
+#if !defined(_WIN32_WCE)
+        if (fileName == NULL) {
+#endif
+            if (fid != NULL && SeekFile64(fid, 0, SEEK_END) >= 0) {
+                fileSize = (u_int64_t)TellFile64(fid);
+                if (fileSize == (u_int64_t)-1)
+                    fileSize = 0; // TellFile64() failed
+                SeekFile64(fid, 0, SEEK_SET);
+            }
+#if !defined(_WIN32_WCE)
+        } else {
+            struct stat sb;
+            if (stat(fileName, &sb) == 0) {
+                fileSize = sb.st_size;
+            }
+        }
+#endif
+    }
+
+    return fileSize;
+}
+
+int64_t SeekFile64(FILE* fid, int64_t offset, int whence)
+{
+    if (fid == NULL)
+        return -1;
+
+    clearerr(fid);
+    fflush(fid);
 #if (defined(__WIN32__) || defined(_WIN32)) && !defined(_WIN32_WCE)
-  return _lseeki64(_fileno(fid), offset, whence) == (int64_t)-1 ? -1 : 0;
+    return _lseeki64(_fileno(fid), offset, whence) == (int64_t)-1 ? -1 : 0;
 #else
 #if defined(_WIN32_WCE)
-  return fseek(fid, (long)(offset), whence);
+    return fseek(fid, (long)(offset), whence);
 #else
-  return fseeko(fid, (off_t)(offset), whence);
+    return fseeko(fid, (off_t)(offset), whence);
 #endif
 #endif
 }
 
-int64_t TellFile64(FILE *fid) {
-  if (fid == NULL) return -1;
+int64_t TellFile64(FILE* fid)
+{
+    if (fid == NULL)
+        return -1;
 
-  clearerr(fid);
-  fflush(fid);
+    clearerr(fid);
+    fflush(fid);
 #if (defined(__WIN32__) || defined(_WIN32)) && !defined(_WIN32_WCE)
-  return _telli64(_fileno(fid));
+    return _telli64(_fileno(fid));
 #else
 #if defined(_WIN32_WCE)
-  return ftell(fid);
+    return ftell(fid);
 #else
-  return ftello(fid);
+    return ftello(fid);
 #endif
 #endif
 }
 
-Boolean FileIsSeekable(FILE *fid) {
-  if (SeekFile64(fid, 1, SEEK_CUR) < 0) {
-    return False;
-  }
+Boolean FileIsSeekable(FILE* fid)
+{
+    if (SeekFile64(fid, 1, SEEK_CUR) < 0) {
+        return False;
+    }
 
-  SeekFile64(fid, -1, SEEK_CUR); // seek back to where we were
-  return True;
+    SeekFile64(fid, -1, SEEK_CUR); // seek back to where we were
+    return True;
 }
--- liveMedia/MatroskaFileParser.hh
+++ liveMedia/MatroskaFileParser.hh
@@ -32,103 +32,103 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 
 // An enum representing the current state of the parser:
 enum MatroskaParseState {
-  PARSING_START_OF_FILE,
-  LOOKING_FOR_TRACKS,
-  PARSING_TRACK,
-  PARSING_CUES,
-  LOOKING_FOR_CLUSTER,
-  LOOKING_FOR_BLOCK,
-  PARSING_BLOCK,
-  DELIVERING_FRAME_WITHIN_BLOCK,
-  DELIVERING_FRAME_BYTES
+    PARSING_START_OF_FILE,
+    LOOKING_FOR_TRACKS,
+    PARSING_TRACK,
+    PARSING_CUES,
+    LOOKING_FOR_CLUSTER,
+    LOOKING_FOR_BLOCK,
+    PARSING_BLOCK,
+    DELIVERING_FRAME_WITHIN_BLOCK,
+    DELIVERING_FRAME_BYTES
 };
 
-class MatroskaFileParser: public StreamParser {
+class MatroskaFileParser : public StreamParser {
 public:
-  MatroskaFileParser(MatroskaFile& ourFile, FramedSource* inputSource,
-		     FramedSource::onCloseFunc* onEndFunc, void* onEndClientData,
-		     MatroskaDemux* ourDemux = NULL);
-  virtual ~MatroskaFileParser();
+    MatroskaFileParser(MatroskaFile& ourFile, FramedSource* inputSource,
+        FramedSource::onCloseFunc* onEndFunc, void* onEndClientData,
+        MatroskaDemux* ourDemux = NULL);
+    virtual ~MatroskaFileParser();
 
-  void seekToTime(double& seekNPT);
+    void seekToTime(double& seekNPT);
 
-  // StreamParser 'client continue' function:
-  static void continueParsing(void* clientData, unsigned char* ptr, unsigned size, struct timeval presentationTime);
-  void continueParsing();
+    // StreamParser 'client continue' function:
+    static void continueParsing(void* clientData, unsigned char* ptr, unsigned size, struct timeval presentationTime);
+    void continueParsing();
 
 private:
-  // Parsing functions:
-  Boolean parse();
+    // Parsing functions:
+    Boolean parse();
     // returns True iff we have finished parsing to the end of all 'Track' headers (on initialization)
 
-  Boolean parseStartOfFile();
-  void lookForNextTrack();
-  Boolean parseTrack();
-  Boolean parseCues();
+    Boolean parseStartOfFile();
+    void lookForNextTrack();
+    Boolean parseTrack();
+    Boolean parseCues();
 
-  void lookForNextBlock();
-  void parseBlock();
-  Boolean deliverFrameWithinBlock();
-  void deliverFrameBytes();
+    void lookForNextBlock();
+    void parseBlock();
+    Boolean deliverFrameWithinBlock();
+    void deliverFrameBytes();
 
-  void getCommonFrameBytes(MatroskaTrack* track, u_int8_t* to, unsigned numBytesToGet, unsigned numBytesToSkip);
+    void getCommonFrameBytes(MatroskaTrack* track, u_int8_t* to, unsigned numBytesToGet, unsigned numBytesToSkip);
 
-  Boolean parseEBMLNumber(EBMLNumber& num);
-  Boolean parseEBMLIdAndSize(EBMLId& id, EBMLDataSize& size);
-  Boolean parseEBMLVal_unsigned64(EBMLDataSize& size, u_int64_t& result);
-  Boolean parseEBMLVal_unsigned(EBMLDataSize& size, unsigned& result);
-  Boolean parseEBMLVal_float(EBMLDataSize& size, float& result);
-  Boolean parseEBMLVal_string(EBMLDataSize& size, char*& result);
+    Boolean parseEBMLNumber(EBMLNumber& num);
+    Boolean parseEBMLIdAndSize(EBMLId& id, EBMLDataSize& size);
+    Boolean parseEBMLVal_unsigned64(EBMLDataSize& size, u_int64_t& result);
+    Boolean parseEBMLVal_unsigned(EBMLDataSize& size, unsigned& result);
+    Boolean parseEBMLVal_float(EBMLDataSize& size, float& result);
+    Boolean parseEBMLVal_string(EBMLDataSize& size, char*& result);
     // Note: "result" is dynamically allocated; the caller must delete[] it later
-  Boolean parseEBMLVal_binary(EBMLDataSize& size, u_int8_t*& result);
+    Boolean parseEBMLVal_binary(EBMLDataSize& size, u_int8_t*& result);
     // Note: "result" is dynamically allocated; the caller must delete[] it later
-  void skipHeader(EBMLDataSize const& size);
-  void skipRemainingHeaderBytes(Boolean isContinuation);
+    void skipHeader(EBMLDataSize const& size);
+    void skipRemainingHeaderBytes(Boolean isContinuation);
 
-  void setParseState();
+    void setParseState();
 
-  void seekToFilePosition(u_int64_t offsetInFile);
-  void seekToEndOfFile();
-  void resetStateAfterSeeking(); // common code, called by both of the above
+    void seekToFilePosition(u_int64_t offsetInFile);
+    void seekToEndOfFile();
+    void resetStateAfterSeeking(); // common code, called by both of the above
 
 private: // redefined virtual functions
-  virtual void restoreSavedParserState();
+    virtual void restoreSavedParserState();
 
 private:
-  // General state for parsing:
-  MatroskaFile& fOurFile;
-  FramedSource* fInputSource;
-  FramedSource::onCloseFunc* fOnEndFunc;
-  void* fOnEndClientData;
-  MatroskaDemux* fOurDemux;
-  MatroskaParseState fCurrentParseState;
-  u_int64_t fCurOffsetInFile, fSavedCurOffsetInFile, fLimitOffsetInFile;
+    // General state for parsing:
+    MatroskaFile& fOurFile;
+    FramedSource* fInputSource;
+    FramedSource::onCloseFunc* fOnEndFunc;
+    void* fOnEndClientData;
+    MatroskaDemux* fOurDemux;
+    MatroskaParseState fCurrentParseState;
+    u_int64_t fCurOffsetInFile, fSavedCurOffsetInFile, fLimitOffsetInFile;
 
-  // For skipping over (possibly large) headers:
-  u_int64_t fNumHeaderBytesToSkip;
+    // For skipping over (possibly large) headers:
+    u_int64_t fNumHeaderBytesToSkip;
 
-  // For parsing 'Seek ID's:
-  EBMLId fLastSeekId;
+    // For parsing 'Seek ID's:
+    EBMLId fLastSeekId;
 
-  // Parameters of the most recently-parsed 'Cluster':
-  unsigned fClusterTimecode;
+    // Parameters of the most recently-parsed 'Cluster':
+    unsigned fClusterTimecode;
 
-  // Parameters of the most recently-parsed 'Block':
-  unsigned fBlockSize;
-  unsigned fBlockTrackNumber;
-  short fBlockTimecode;
-  unsigned fNumFramesInBlock;
-  unsigned* fFrameSizesWithinBlock;
+    // Parameters of the most recently-parsed 'Block':
+    unsigned fBlockSize;
+    unsigned fBlockTrackNumber;
+    short fBlockTimecode;
+    unsigned fNumFramesInBlock;
+    unsigned* fFrameSizesWithinBlock;
 
-  // Parameters of the most recently-parsed frame within a 'Block':
-  double fPresentationTimeOffset;
-  unsigned fNextFrameNumberToDeliver;
-  unsigned fCurOffsetWithinFrame, fSavedCurOffsetWithinFrame; // used if track->haveSubframes()
+    // Parameters of the most recently-parsed frame within a 'Block':
+    double fPresentationTimeOffset;
+    unsigned fNextFrameNumberToDeliver;
+    unsigned fCurOffsetWithinFrame, fSavedCurOffsetWithinFrame; // used if track->haveSubframes()
 
-  // Parameters of the (sub)frame that's currently being delivered:
-  u_int8_t* fCurFrameTo;
-  unsigned fCurFrameNumBytesToGet;
-  unsigned fCurFrameNumBytesToSkip;
+    // Parameters of the (sub)frame that's currently being delivered:
+    u_int8_t* fCurFrameTo;
+    unsigned fCurFrameNumBytesToGet;
+    unsigned fCurFrameNumBytesToSkip;
 };
 
 #endif
