7baea3b4105d827a9ed20dd65d5159789a586bc2
longcp <1784857911@qq.com>
2018-01-29 01:26:37 +0800

    文件格式对齐

--- groupsock/include/Groupsock.hh
+++ groupsock/include/Groupsock.hh
@@ -36,123 +36,126 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 // An "OutputSocket" is (by default) used only to send packets.
 // No packets are received on it (unless a subclass arranges this)
 
-class OutputSocket: public Socket {
+class OutputSocket : public Socket {
 public:
-  OutputSocket(UsageEnvironment& env);
-  virtual ~OutputSocket();
+    OutputSocket(UsageEnvironment& env);
+    virtual ~OutputSocket();
 
-  Boolean write(netAddressBits address, Port port, u_int8_t ttl,
-		unsigned char* buffer, unsigned bufferSize);
+    Boolean write(netAddressBits address, Port port, u_int8_t ttl,
+        unsigned char* buffer, unsigned bufferSize);
 
 protected:
-  OutputSocket(UsageEnvironment& env, Port port);
+    OutputSocket(UsageEnvironment& env, Port port);
 
-  portNumBits sourcePortNum() const {return fSourcePort.num();}
+    portNumBits sourcePortNum() const { return fSourcePort.num(); }
 
 private: // redefined virtual function
-  virtual Boolean handleRead(unsigned char* buffer, unsigned bufferMaxSize,
-			     unsigned& bytesRead,
-			     struct sockaddr_in& fromAddress);
+    virtual Boolean handleRead(unsigned char* buffer, unsigned bufferMaxSize,
+        unsigned& bytesRead,
+        struct sockaddr_in& fromAddress);
 
 private:
-  Port fSourcePort;
-  unsigned fLastSentTTL;
+    Port fSourcePort;
+    unsigned fLastSentTTL;
 };
 
 class destRecord {
 public:
-  destRecord(struct in_addr const& addr, Port const& port, u_int8_t ttl,
-	     destRecord* next);
-  virtual ~destRecord();
+    destRecord(struct in_addr const& addr, Port const& port, u_int8_t ttl,
+        destRecord* next);
+    virtual ~destRecord();
 
 public:
-  destRecord* fNext;
-  GroupEId fGroupEId;
-  Port fPort;
+    destRecord* fNext;
+    GroupEId fGroupEId;
+    Port fPort;
 };
 
 // A "Groupsock" is used to both send and receive packets.
 // As the name suggests, it was originally designed to send/receive
 // multicast, but it can send/receive unicast as well.
 
-class Groupsock: public OutputSocket {
+class Groupsock : public OutputSocket {
 public:
-  Groupsock(UsageEnvironment& env, struct in_addr const& groupAddr,
-	    Port port, u_int8_t ttl);
-      // used for a 'source-independent multicast' group
-  Groupsock(UsageEnvironment& env, struct in_addr const& groupAddr,
-	    struct in_addr const& sourceFilterAddr,
-	    Port port);
-      // used for a 'source-specific multicast' group
-  virtual ~Groupsock();
+    Groupsock(UsageEnvironment& env, struct in_addr const& groupAddr,
+        Port port, u_int8_t ttl);
+    // used for a 'source-independent multicast' group
+    Groupsock(UsageEnvironment& env, struct in_addr const& groupAddr,
+        struct in_addr const& sourceFilterAddr,
+        Port port);
+    // used for a 'source-specific multicast' group
+    virtual ~Groupsock();
 
-  void changeDestinationParameters(struct in_addr const& newDestAddr,
-				   Port newDestPort, int newDestTTL);
-      // By default, the destination address, port and ttl for
-      // outgoing packets are those that were specified in
-      // the constructor.  This works OK for multicast sockets,
-      // but for unicast we usually want the destination port
-      // number, at least, to be different from the source port.
-      // (If a parameter is 0 (or ~0 for ttl), then no change made.)
+    void changeDestinationParameters(struct in_addr const& newDestAddr,
+        Port newDestPort, int newDestTTL);
+    // By default, the destination address, port and ttl for
+    // outgoing packets are those that were specified in
+    // the constructor.  This works OK for multicast sockets,
+    // but for unicast we usually want the destination port
+    // number, at least, to be different from the source port.
+    // (If a parameter is 0 (or ~0 for ttl), then no change made.)
 
-  // As a special case, we also allow multiple destinations (addresses & ports)
-  // (This can be used to implement multi-unicast.)
-  void addDestination(struct in_addr const& addr, Port const& port);
-  void removeDestination(struct in_addr const& addr, Port const& port);
-  void removeAllDestinations();
+    // As a special case, we also allow multiple destinations (addresses & ports)
+    // (This can be used to implement multi-unicast.)
+    void addDestination(struct in_addr const& addr, Port const& port);
+    void removeDestination(struct in_addr const& addr, Port const& port);
+    void removeAllDestinations();
 
-  struct in_addr const& groupAddress() const {
-    return fIncomingGroupEId.groupAddress();
-  }
-  struct in_addr const& sourceFilterAddress() const {
-    return fIncomingGroupEId.sourceFilterAddress();
-  }
+    struct in_addr const& groupAddress() const
+    {
+        return fIncomingGroupEId.groupAddress();
+    }
+    struct in_addr const& sourceFilterAddress() const
+    {
+        return fIncomingGroupEId.sourceFilterAddress();
+    }
 
-  Boolean isSSM() const {
-    return fIncomingGroupEId.isSSM();
-  }
+    Boolean isSSM() const
+    {
+        return fIncomingGroupEId.isSSM();
+    }
 
-  u_int8_t ttl() const { return fTTL; }
+    u_int8_t ttl() const { return fTTL; }
 
-  void multicastSendOnly(); // send, but don't receive any multicast packets
+    void multicastSendOnly(); // send, but don't receive any multicast packets
 
-  virtual Boolean output(UsageEnvironment& env, u_int8_t ttl,
-			 unsigned char* buffer, unsigned bufferSize,
-			 DirectedNetInterface* interfaceNotToFwdBackTo = NULL);
+    virtual Boolean output(UsageEnvironment& env, u_int8_t ttl,
+        unsigned char* buffer, unsigned bufferSize,
+        DirectedNetInterface* interfaceNotToFwdBackTo = NULL);
 
-  DirectedNetInterfaceSet& members() { return fMembers; }
+    DirectedNetInterfaceSet& members() { return fMembers; }
 
-  Boolean deleteIfNoMembers;
-  Boolean isSlave; // for tunneling
+    Boolean deleteIfNoMembers;
+    Boolean isSlave; // for tunneling
 
-  static NetInterfaceTrafficStats statsIncoming;
-  static NetInterfaceTrafficStats statsOutgoing;
-  static NetInterfaceTrafficStats statsRelayedIncoming;
-  static NetInterfaceTrafficStats statsRelayedOutgoing;
-  NetInterfaceTrafficStats statsGroupIncoming; // *not* static
-  NetInterfaceTrafficStats statsGroupOutgoing; // *not* static
-  NetInterfaceTrafficStats statsGroupRelayedIncoming; // *not* static
-  NetInterfaceTrafficStats statsGroupRelayedOutgoing; // *not* static
+    static NetInterfaceTrafficStats statsIncoming;
+    static NetInterfaceTrafficStats statsOutgoing;
+    static NetInterfaceTrafficStats statsRelayedIncoming;
+    static NetInterfaceTrafficStats statsRelayedOutgoing;
+    NetInterfaceTrafficStats statsGroupIncoming; // *not* static
+    NetInterfaceTrafficStats statsGroupOutgoing; // *not* static
+    NetInterfaceTrafficStats statsGroupRelayedIncoming; // *not* static
+    NetInterfaceTrafficStats statsGroupRelayedOutgoing; // *not* static
 
-  Boolean wasLoopedBackFromUs(UsageEnvironment& env,
-			      struct sockaddr_in& fromAddress);
+    Boolean wasLoopedBackFromUs(UsageEnvironment& env,
+        struct sockaddr_in& fromAddress);
 
 public: // redefined virtual functions
-  virtual Boolean handleRead(unsigned char* buffer, unsigned bufferMaxSize,
-			     unsigned& bytesRead,
-			     struct sockaddr_in& fromAddress);
+    virtual Boolean handleRead(unsigned char* buffer, unsigned bufferMaxSize,
+        unsigned& bytesRead,
+        struct sockaddr_in& fromAddress);
 
 private:
-  int outputToAllMembersExcept(DirectedNetInterface* exceptInterface,
-			       u_int8_t ttlToFwd,
-			       unsigned char* data, unsigned size,
-			       netAddressBits sourceAddr);
+    int outputToAllMembersExcept(DirectedNetInterface* exceptInterface,
+        u_int8_t ttlToFwd,
+        unsigned char* data, unsigned size,
+        netAddressBits sourceAddr);
 
 private:
-  GroupEId fIncomingGroupEId;
-  destRecord* fDests;
-  u_int8_t fTTL;
-  DirectedNetInterfaceSet fMembers;
+    GroupEId fIncomingGroupEId;
+    destRecord* fDests;
+    u_int8_t fTTL;
+    DirectedNetInterfaceSet fMembers;
 };
 
 UsageEnvironment& operator<<(UsageEnvironment& s, const Groupsock& g);
@@ -161,43 +164,43 @@ UsageEnvironment& operator<<(UsageEnvironment& s, const Groupsock& g);
 // by (multicast address, port), or by socket number
 class GroupsockLookupTable {
 public:
-  Groupsock* Fetch(UsageEnvironment& env, netAddressBits groupAddress,
-		   Port port, u_int8_t ttl, Boolean& isNew);
-      // Creates a new Groupsock if none already exists
-  Groupsock* Fetch(UsageEnvironment& env, netAddressBits groupAddress,
-		   netAddressBits sourceFilterAddr,
-		   Port port, Boolean& isNew);
-      // Creates a new Groupsock if none already exists
-  Groupsock* Lookup(netAddressBits groupAddress, Port port);
-      // Returns NULL if none already exists
-  Groupsock* Lookup(netAddressBits groupAddress,
-		    netAddressBits sourceFilterAddr,
-		    Port port);
-      // Returns NULL if none already exists
-  Groupsock* Lookup(UsageEnvironment& env, int sock);
-      // Returns NULL if none already exists
-  Boolean Remove(Groupsock const* groupsock);
+    Groupsock* Fetch(UsageEnvironment& env, netAddressBits groupAddress,
+        Port port, u_int8_t ttl, Boolean& isNew);
+    // Creates a new Groupsock if none already exists
+    Groupsock* Fetch(UsageEnvironment& env, netAddressBits groupAddress,
+        netAddressBits sourceFilterAddr,
+        Port port, Boolean& isNew);
+    // Creates a new Groupsock if none already exists
+    Groupsock* Lookup(netAddressBits groupAddress, Port port);
+    // Returns NULL if none already exists
+    Groupsock* Lookup(netAddressBits groupAddress,
+        netAddressBits sourceFilterAddr,
+        Port port);
+    // Returns NULL if none already exists
+    Groupsock* Lookup(UsageEnvironment& env, int sock);
+    // Returns NULL if none already exists
+    Boolean Remove(Groupsock const* groupsock);
 
-  // Used to iterate through the groupsocks in the table
-  class Iterator {
-  public:
-    Iterator(GroupsockLookupTable& groupsocks);
+    // Used to iterate through the groupsocks in the table
+    class Iterator {
+    public:
+        Iterator(GroupsockLookupTable& groupsocks);
 
-    Groupsock* next(); // NULL iff none
+        Groupsock* next(); // NULL iff none
 
-  private:
-    AddressPortLookupTable::Iterator fIter;
-  };
-
-private:
-  Groupsock* AddNew(UsageEnvironment& env,
-		    netAddressBits groupAddress,
-		    netAddressBits sourceFilterAddress,
-		    Port port, u_int8_t ttl);
+    private:
+        AddressPortLookupTable::Iterator fIter;
+    };
 
 private:
-  friend class Iterator;
-  AddressPortLookupTable fTable;
+    Groupsock* AddNew(UsageEnvironment& env,
+        netAddressBits groupAddress,
+        netAddressBits sourceFilterAddress,
+        Port port, u_int8_t ttl);
+
+private:
+    friend class Iterator;
+    AddressPortLookupTable fTable;
 };
 
 #endif
--- groupsock/include/NetInterface.hh
+++ groupsock/include/NetInterface.hh
@@ -27,85 +27,88 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 
 class NetInterface {
 public:
-  virtual ~NetInterface();
+    virtual ~NetInterface();
 
-  static UsageEnvironment* DefaultUsageEnvironment;
-      // if non-NULL, used for each new interfaces
+    static UsageEnvironment* DefaultUsageEnvironment;
+    // if non-NULL, used for each new interfaces
 
 protected:
-  NetInterface(); // virtual base class
+    NetInterface(); // virtual base class
 };
 
-class DirectedNetInterface: public NetInterface {
+class DirectedNetInterface : public NetInterface {
 public:
-  virtual ~DirectedNetInterface();
+    virtual ~DirectedNetInterface();
 
-  virtual Boolean write(unsigned char* data, unsigned numBytes) = 0;
+    virtual Boolean write(unsigned char* data, unsigned numBytes) = 0;
 
-  virtual Boolean SourceAddrOKForRelaying(UsageEnvironment& env,
-					  unsigned addr) = 0;
+    virtual Boolean SourceAddrOKForRelaying(UsageEnvironment& env,
+        unsigned addr)
+        = 0;
 
 protected:
-  DirectedNetInterface(); // virtual base class
+    DirectedNetInterface(); // virtual base class
 };
 
 class DirectedNetInterfaceSet {
 public:
-  DirectedNetInterfaceSet();
-  virtual ~DirectedNetInterfaceSet();
+    DirectedNetInterfaceSet();
+    virtual ~DirectedNetInterfaceSet();
 
-  DirectedNetInterface* Add(DirectedNetInterface const* interf);
-      // Returns the old value if different, otherwise 0
-  Boolean Remove(DirectedNetInterface const* interf);
+    DirectedNetInterface* Add(DirectedNetInterface const* interf);
+    // Returns the old value if different, otherwise 0
+    Boolean Remove(DirectedNetInterface const* interf);
 
-  Boolean IsEmpty() { return fTable->IsEmpty(); }
+    Boolean IsEmpty() { return fTable->IsEmpty(); }
 
-  // Used to iterate through the interfaces in the set
-  class Iterator {
-  public:
-    Iterator(DirectedNetInterfaceSet& interfaces);
-    virtual ~Iterator();
+    // Used to iterate through the interfaces in the set
+    class Iterator {
+    public:
+        Iterator(DirectedNetInterfaceSet& interfaces);
+        virtual ~Iterator();
 
-    DirectedNetInterface* next(); // NULL iff none
+        DirectedNetInterface* next(); // NULL iff none
 
-  private:
-    HashTable::Iterator* fIter;
-  };
+    private:
+        HashTable::Iterator* fIter;
+    };
 
 private:
-  friend class Iterator;
-  HashTable* fTable;
+    friend class Iterator;
+    HashTable* fTable;
 };
 
-class Socket: public NetInterface {
+class Socket : public NetInterface {
 public:
-  virtual ~Socket();
-  void reset(); // closes the socket, and sets "fSocketNum" to -1
+    virtual ~Socket();
+    void reset(); // closes the socket, and sets "fSocketNum" to -1
 
-  virtual Boolean handleRead(unsigned char* buffer, unsigned bufferMaxSize,
-			     unsigned& bytesRead,
-			     struct sockaddr_in& fromAddress) = 0;
-      // Returns False on error; resultData == NULL if data ignored
+    virtual Boolean handleRead(unsigned char* buffer, unsigned bufferMaxSize,
+        unsigned& bytesRead,
+        struct sockaddr_in& fromAddress)
+        = 0;
+    // Returns False on error; resultData == NULL if data ignored
 
-  int socketNum() const { return fSocketNum; }
+    int socketNum() const { return fSocketNum; }
 
-  Port port() const {
-    return fPort;
-  }
+    Port port() const
+    {
+        return fPort;
+    }
 
-  UsageEnvironment& env() const { return fEnv; }
+    UsageEnvironment& env() const { return fEnv; }
 
-  static int DebugLevel;
+    static int DebugLevel;
 
 protected:
-  Socket(UsageEnvironment& env, Port port); // virtual base class
+    Socket(UsageEnvironment& env, Port port); // virtual base class
 
-  Boolean changePort(Port newPort); // will also cause socketNum() to change
+    Boolean changePort(Port newPort); // will also cause socketNum() to change
 
 private:
-  int fSocketNum;
-  UsageEnvironment& fEnv;
-  Port fPort;
+    int fSocketNum;
+    UsageEnvironment& fEnv;
+    Port fPort;
 };
 
 UsageEnvironment& operator<<(UsageEnvironment& s, const Socket& sock);
@@ -114,36 +117,36 @@ UsageEnvironment& operator<<(UsageEnvironment& s, const Socket& sock);
 
 class SocketLookupTable {
 public:
-  virtual ~SocketLookupTable();
+    virtual ~SocketLookupTable();
 
-  Socket* Fetch(UsageEnvironment& env, Port port, Boolean& isNew);
-  // Creates a new Socket if none already exists
-  Boolean Remove(Socket const* sock);
+    Socket* Fetch(UsageEnvironment& env, Port port, Boolean& isNew);
+    // Creates a new Socket if none already exists
+    Boolean Remove(Socket const* sock);
 
 protected:
-  SocketLookupTable(); // abstract base class
-  virtual Socket* CreateNew(UsageEnvironment& env, Port port) = 0;
+    SocketLookupTable(); // abstract base class
+    virtual Socket* CreateNew(UsageEnvironment& env, Port port) = 0;
 
 private:
-  HashTable* fTable;
+    HashTable* fTable;
 };
 
 // A data structure for counting traffic:
 
 class NetInterfaceTrafficStats {
 public:
-  NetInterfaceTrafficStats();
+    NetInterfaceTrafficStats();
 
-  void countPacket(unsigned packetSize);
+    void countPacket(unsigned packetSize);
 
-  float totNumPackets() const {return fTotNumPackets;}
-  float totNumBytes() const {return fTotNumBytes;}
+    float totNumPackets() const { return fTotNumPackets; }
+    float totNumBytes() const { return fTotNumBytes; }
 
-  Boolean haveSeenTraffic() const;
+    Boolean haveSeenTraffic() const;
 
 private:
-  float fTotNumPackets;
-  float fTotNumBytes;
+    float fTotNumPackets;
+    float fTotNumBytes;
 };
 
 #endif
